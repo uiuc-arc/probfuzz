e260d0222e940b051343eb7bfe53b8ebbe6cb07e
yekerr <zixinh2@illinois.edu>
2018-10-22 19:57:57 +0000

    touch __init__.py in language/antlr

--- language/antlr/run.sh
+++ language/antlr/run.sh
@@ -2,3 +2,4 @@
 antlr4='java -Xmx500M -cp ".:./antlr-4.7.1-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
 grun='java org.antlr.v4.gui.TestRig'
 $antlr4 -package "tool.parser"  -Dlanguage=Python2 -visitor Template.g4
+touch __init__.py
